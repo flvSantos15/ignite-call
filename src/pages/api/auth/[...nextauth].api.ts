892a9162ac00a0260873fea1181080b869511ab2
flvSantos15 <flvSantos300@gmail.com>
2023-04-10 23:05:36 -0300

    feat: finishing prisma adapter

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
+import { PrismaAdapter } from '@/lib/auth/prisma-adapter'
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
