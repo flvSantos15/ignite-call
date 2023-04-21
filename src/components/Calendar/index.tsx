ad155d4a5305744803b35284291bb00221821bc1
flvSantos15 <flvSantos300@gmail.com>
2023-04-20 21:43:51 -0300

    feat: calendar style

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -47,7 +47,7 @@ export function Calendar() {
               <CalendarDay>1</CalendarDay>
             </td>
             <td>
-              <CalendarDay>2</CalendarDay>
+              <CalendarDay disabled>2</CalendarDay>
             </td>
             <td>
               <CalendarDay>3</CalendarDay>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -6,5 +6,7 @@ export const Container = styled('div', {
   padding: 0,
   display: 'grid',
   maxWidth: '100%',
-  position: 'relative'
+  position: 'relative',
+  width: 540,
+  gridTemplateColumns: '1fr'
 })
