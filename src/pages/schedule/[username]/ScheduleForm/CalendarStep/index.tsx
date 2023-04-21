7ee1275967a059719cc548e9cc70fa181751394a
flvSantos15 <flvSantos300@gmail.com>
2023-04-21 08:18:27 -0300

    feat: schedules list estructure

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,40 @@
+/* eslint-disable prettier/prettier */
 import { Calendar } from '@/components/Calendar'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList
+} from './styles'
 
 export function CalendarStep() {
+  const isDaySelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDaySelected}>
       <Calendar />
+      {isDaySelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            sexta-feira <span>21 de abril</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+            <TimePickerItem>13:00h</TimePickerItem>
+            <TimePickerItem>14:00h</TimePickerItem>
+            <TimePickerItem>15:00h</TimePickerItem>
+            <TimePickerItem>16:00h</TimePickerItem>
+            <TimePickerItem>17:00h</TimePickerItem>
+            <TimePickerItem>18:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
