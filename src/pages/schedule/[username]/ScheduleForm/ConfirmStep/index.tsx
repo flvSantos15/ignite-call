78d227700d534be30e6ec8bdab04b233b3c44b9e
flvSantos15 <flvSantos300@gmail.com>
2023-04-22 12:04:54 -0300

    feat: confirm page estructure

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,44 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+
+export function ConfirmStep() {
+  const handleConfirmaSchedule = async () => {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleConfirmaSchedule}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="jondoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observação</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
