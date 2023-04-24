a1277dd82c6b98500323afd9864f2722cf4e52ef
flvSantos15 <flvSantos300@gmail.com>
2023-04-23 21:16:22 -0300

    feat:appoitments table

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -8,11 +8,17 @@ import {
   TimePickerList
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDaySelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDaySelected}>
@@ -21,7 +27,7 @@ export function CalendarStep() {
       {isDaySelected && (
         <TimePicker>
           <TimePickerHeader>
-            sexta-feira <span>21 de abril</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
 
           <TimePickerList>
